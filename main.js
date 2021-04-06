a0334453938fb3229612b10a86e42e3fdfae2dd2
Michael Luu <michaelluu02@protonmail.com>
2021-04-06 22:15:36 +0200

    Add game function, to play n rounds

--- main.js
+++ main.js
@@ -1,100 +1,172 @@
 let options = ["Rock", "Paper", "Scissors"];
 
-play();
 
-function play() {
-    playRound(computerPlay(), playerPlay());
-}
+game(5);
+
 
 function random(input) {
+
+    // generates random number from 0 to input
     return Math.floor(Math.random() * input);
 }
+
 
 function computerPlay() {
     let option = random(3);
     return options[option];
 }
 
+
 function playerPlay() {
     return prompt(`Choose "Rock", "Paper" or "Scissors"`);
 }
 
-function playRound(computerSelection, playerSelection) {
 
+function decideWinner(computerSelection, playerSelection) {
 
-    let output = "";
-
-    // Boolean didWin if player won / not / draw
     let didWin;
 
-
-    if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
-
-
-        // if draw
-        output = "It's a draw! Nobody wins!";
-
-
-    } else if (playerSelection.toLowerCase() == "rock") {
+    if (playerSelection.toLowerCase() == "rock") {
 
 
         if (computerSelection == "Paper") {
             didWin = false;
-            output = `${computerSelection} beats ${playerSelection}!`;
 
 
         } else if (computerSelection == "Scissors") {
             didWin = true;
-            output = `${playerSelection} beats ${computerSelection}!`;
         }
 
 
 
-    } else if (playerSelection.toLowerCase() == "paper") {
+    }
+    if (playerSelection.toLowerCase() == "paper") {
 
 
         if (computerSelection == "Scissors") {
             didWin = false;
-            output = `${computerSelection} beats ${playerSelection}!`;
 
 
         } else if (computerSelection == "Rock") {
             didWin = true;
-            output = `${playerSelection} beats ${computerSelection}!`;
         }
 
 
 
-    } else if (playerSelection.toLowerCase() == "scissors") {
+    }
+    if (playerSelection.toLowerCase() == "scissors") {
 
 
         if (computerSelection == "Rock") {
             didWin = false;
-            output = `${computerSelection} beats ${playerSelection}!`;
 
 
         } else if (computerSelection == "Paper") {
             didWin = true;
-            output = `${playerSelection} beats ${computerSelection}!`;
         }
 
 
+    }
 
-    } else {
+    return didWin;
+} // end of decideWinner()
 
-        // if none of the options match
-        output = "Invalid choice!";
 
-    } // end of if-else
+
+function playRound(computerSelection, playerSelection) {
+
+
+    // Boolean didWin if player won / not / draw
+    let didWin;
+    let output = "";
+
+
+    didWin = decideWinner(computerSelection, playerSelection);
 
 
     if (didWin) {
-        output = `You Win! ${output}`;
+        output = `You Win! ${playerSelection} beats ${computerSelection}`;
     }
+
+
     if (didWin === false) {
-        output = `You Lose! ${output}`;
+        output = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+
+
+    // if didWin = undefined then it's a 
+    // draw or an invalid player-choice
+    if (didWin === undefined) {
+
+
+        if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+
+
+            // if draw
+            output = "It's a draw! Nobody wins!";
+
+
+        } else {
+
+
+            output = "Invalid choice!";
+
+
+        }
     }
 
 
     console.log(output);
+
+    return didWin;
+} // end of playRound() 
+
+
+
+function play() {
+    return playRound(computerPlay(), playerPlay());
 }
+
+
+
+function game(gamesCount = 1) {
+
+
+    let roundsWon = 0,
+        roundsLost = 0,
+        output = "";
+
+
+    for (let i = 0; i < gamesCount; i++) {
+
+
+        let didWin = play();
+
+
+        if (didWin)
+            roundsWon++;
+
+        if (didWin === false)
+            roundsLost++;
+
+
+        // if didWin = undefined, noone scores
+    }
+
+
+    if (roundsWon > roundsLost)
+        output = "won"
+
+    else if (roundsWon < roundsLost)
+        output = "lost"
+
+    else
+        output = "tied";
+
+
+
+    output = `You ${output} the game with an end-score of 
+                    ${roundsWon}:${roundsLost}!`;
+
+    console.log(output);
+} // end of game()
